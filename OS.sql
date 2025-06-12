e520ad05f43050f690ce5ae83bc0efebe0b84066
GUSTAVO MALLORQUIN RODRIGUEZ <159266751+GustavoMallorquinRodriguez@users.noreply.github.com>
2025-06-12 08:56:22 -0300

    12/06

---
+++ OS.sql
@@ -0,0 +1,24 @@
+--- 12-06-2025 08:55:05 SQLite
+/***** ERROR ******
+CREATE TABLE IF NOT EXISTS OS (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_moto INTEGER,
+  	os_dt_ini DATABASE,
+  	os_dt_fim DATABASE,
+    os_valtot VARCHAR(100),
+    os_status varchar(10)
+);
+
+INSERT INTO OS (id_moto, os_dt_ini, os_dt_fim, os_valtot, os_status)
+VALUES (
+	'01',
+  	'09-03-2025',
+	'11-03-2025',
+  	'$150',
+  	'1'
+);
+ ----- 
+SQLITE_ERROR: sqlite3 result code 1: table OS has no column named os_dt_fim
+*****/
+
+
